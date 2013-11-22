6e926fb0731b2bdb1e19bdb8877afe584a859ee2
Albert Martin <albertmartin@mac.com>
2013-11-21 19:02:39 -0500

    Added MD5 NSString helper.

---
+++ PhotoPad/NSString+MD5.h
@@ -0,0 +1,14 @@
+//
+//  NSString+MD5.h
+//  PhotoPad
+//
+//  Courtesy of http://iosdevelopertips.com/core-services/create-md5-hash-from-nsstring-nsdata-or-file.html
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(MD5)
+
+- (NSString *)MD5;
+
+@end
